a272eadcf9273c801447de7fcd644e9e1927a4f5
dcfernandez1023 <54515508+dcfernandez1023@users.noreply.github.com>
2021-05-22 23:13:25 -0700

    update app

--- training-diary-backend/app.py
+++ training-diary-backend/app.py
@@ -1,21 +1,16 @@
 from flask import Flask
-
-
 from models import DataAccess
+from controllers import auth_controller
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def test_db_connect():
-    try:
-        db = DataAccess.DataAccess()
-        db.get({"_id": "init"}, "users")
-        return "Successfully connected to database"
-    except Exception:
-        return "Failed to connect to database"
+def index():
+    return "404 Not Found"
 
 
 if __name__ == "__main__":
+    app.register_blueprint(auth_controller.AUTH_BLUEPRINT)
     app.run(debug=True, host='0.0.0.0', port=5000)
